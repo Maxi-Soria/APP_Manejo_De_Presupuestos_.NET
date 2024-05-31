19d81579a93b5b858bf8b8c64083e565efad4660
Maximiliano Facundo Soria <107376486+Maxi-Soria@users.noreply.github.com>
2024-05-31 01:51:18 -0300

    Subida del proyecto base con la vista TipoCuentas

---
+++ ManejoPresupuesto/ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,29 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+
+            return View();
+        }
+
+
+    }
+}
---
+++ ManejoPresupuesto/ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo nombre es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo nombre debe estar entre {2} y {1}")]
+
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+    }
+}
